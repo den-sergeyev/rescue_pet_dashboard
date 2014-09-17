8241cb88f9fee81f24e1a303bf845c61de697264
Dzianis Siarheyeu <dzianis_siarheyeu@epam.com>
2014-09-17 12:02:25 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RescuePerDashboard::Application.config.secret_key_base = 'ad64aebc76acb18551507e8564cf8a7dba1f61590b2dfa5b8744d89a11de40991c2622f904639154c82fdcb68902186feeda2c8c4c12adf440a07d0e664ec8e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RescuePerDashboard::Application.config.session_store :cookie_store, key: '_rescue_per_dashboard_session'
